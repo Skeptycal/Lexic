3d7a7ea4f279463e248a0cc8898e06043478ddde
Benjamin <benjadahl@gmail.com>
2016-05-27 15:54:57 +0200

    Added horisontal labels, for the predictions

--- GUI/LexicGUILauncher.py
+++ GUI/LexicGUILauncher.py
@@ -4,7 +4,20 @@ import gi
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
 
-win = Gtk.Window()
+class myWindow (Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="LexicGUITest")
+
+        self.box = Gtk.Box(spacing=6)
+        self.add(self.box)
+
+        self.label1 = Gtk.Label("Label1")
+        self.box.pack_start(self.label1, True, True, 0)
+
+        self.label2 = Gtk.Label("Label2")
+        self.box.pack_start(self.label2, True, True, 0)
+
+win = myWindow()
 
 win.connect("delete-event", Gtk.main_quit)
 
