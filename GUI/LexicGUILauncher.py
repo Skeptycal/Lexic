70f1b07fcc0bf6e80481a79d590019902461a1f7
benjadahl <benjadahl>
2016-05-28 00:51:22 +0200

    Made the GUI remotely work, yet the code needs more polishing

--- GUI/LexicGUILauncher.py
+++ GUI/LexicGUILauncher.py
@@ -8,18 +8,57 @@ class myWindow (Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="LexicGUITest")
 
-        self.box = Gtk.Box(spacing=6)
+        global WindowVar
+        WindowVar = self
+
+        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(self.box)
 
-        self.label1 = Gtk.Label("Label1")
-        self.box.pack_start(self.label1, True, True, 0)
+        self.prediction1 = Gtk.Button(label = "Prediction #1")
+        self.box.pack_start(self.prediction1, True, True, 0)
 
-        self.label2 = Gtk.Label("Label2")
-        self.box.pack_start(self.label2, True, True, 0)
+        self.prediction2 = Gtk.Button(label = "Prediction #2")
+        self.box.pack_start(self.prediction2, True, True, 0)
+
+        self.prediction3 = Gtk.Button(label = "Prediction #3")
+        self.box.pack_start(self.prediction3, True, True, 0)
+
+        self.prediction4 = Gtk.Button(label = "Prediction #4")
+        self.box.pack_start(self.prediction4, True, True, 0)
+
+        self.prediction5 = Gtk.Button(label = "Prediction #5")
+        self.box.pack_start(self.prediction5, True, True, 0)
+
+        self.prediction6 = Gtk.Button(label = "Prediction #6")
+        self.box.pack_start(self.prediction6, True, True, 0)
+
+        self.prediction7 = Gtk.Button(label = "Prediction #7")
+        self.box.pack_start(self.prediction7, True, True, 0)
+
+        self.prediction8 = Gtk.Button(label = "Prediction #8")
+        self.box.pack_start(self.prediction8, True, True, 0)
+
+        self.prediction9 = Gtk.Button(label = "Prediction #9")
+        self.box.pack_start(self.prediction9, True, True, 0)
+
+def test(widget):
+    print "lel"
 
 win = myWindow()
 
 win.connect("delete-event", Gtk.main_quit)
+
+win.prediction1.connect("clicked", test)
+win.prediction2.connect("clicked", test)
+win.prediction3.connect("clicked", test)
+win.prediction4.connect("clicked", test)
+win.prediction5.connect("clicked", test)
+win.prediction6.connect("clicked", test)
+win.prediction7.connect("clicked", test)
+win.prediction8.connect("clicked", test)
+win.prediction9.connect("clicked", test)
+
+
 
 win.show_all()
 
