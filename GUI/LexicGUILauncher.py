f339e961131e46bc5ec0946a283e41f7a45cc879
benjadahl <benjadahl>
2016-05-29 01:08:26 +0200

    Added updateSuggestions function

--- GUI/LexicGUILauncher.py
+++ GUI/LexicGUILauncher.py
@@ -5,10 +5,12 @@ import gi
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
 
-#Customizable variables
+#Customizable variables |---
 
 #The amount of buttons that will be created:
 buttonAmount = 9
+
+#---|
 
 #Create class for the main window
 class mainWindow (Gtk.Window):
@@ -26,8 +28,9 @@ win = mainWindow()
 
 #Function for creating the buttons
 def createButtons(amount):
+    print "[GUI] Creating " + str(amount) + " buttons"
     i = 1
-    while i <= amount :
+    while i <= amount:
         #Add the buttons to the window class
         setattr(win, "prediction" + str(i), Gtk.Button(label = "Prediction #" + str(i)))
         #Add the buttons to the box
@@ -36,8 +39,22 @@ def createButtons(amount):
         getattr(win, "prediction" + str(i)).connect("clicked", on_prediction_click, i)
         i += 1
 
+def updateSuggestions(suggestionList, bAmount):
+    print "[GUI] Updating suggestions"
+
+    try:
+        i = 0
+        while i <= bAmount:
+            if i < len(suggestionList):
+                getattr(win, "prediction" + str(i + 1)).set_label(suggestionList[i])
+                i += 1
+            else:
+                return
+    except:
+        print "[GUI] Error during suggestion update (might be due to input not being a list)"
+
 def on_prediction_click(widget, sn):
-    print "Suggestion #" + str(sn) + " was pressed."
+    print "[GUI] Suggestion #" + str(sn) + " was pressed."
     #Return userchoice to script that emulates keypresses.
 
 #Connect the closing of the main window, to ending the script with Gtk.main_quit
@@ -48,6 +65,8 @@ createButtons(buttonAmount)
 
 #Show the window and it's children(buttons)
 win.show_all()
+
+updateSuggestions(["example1","example2","example3"], buttonAmount)
 
 #Executes the main function of Gtk
 Gtk.main()
