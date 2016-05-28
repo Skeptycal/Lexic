8810546b7aa2d906ff17dab7c2e3f0ee352e3918
benjadahl <benjadahl>
2016-05-28 23:42:29 +0200

    Added comments to the GUI code

--- GUI/LexicGUILauncher.py
+++ GUI/LexicGUILauncher.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 
+#Import dependencies
 import gi
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk
 
 #Customizable variables
+
+#The amount of buttons that will be created:
 buttonAmount = 9
 
+#Create class for the main window
 class mainWindow (Gtk.Window):
+    #Initiating function for the window
     def __init__(self):
         Gtk.Window.__init__(self, title="LexicGUITest")
 
+        #Create vertical box that the buttons will be placed in
         self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
+        #Add the box to the window
         self.add(self.box)
 
 win = mainWindow()
 
-def createButtons():
+#Function for creating the buttons
+def createButtons(amount):
     i = 1
-    while i <= buttonAmount :
+    while i <= amount :
+        #Add the buttons to the window class
         setattr(win, "prediction" + str(i), Gtk.Button(label = "Prediction #" + str(i)))
+        #Add the buttons to the box
         win.box.pack_start(getattr(win, "prediction" + str(i)), True, True, 0)
+        #Connect the buttons clicked event to the on_prediction_click function
         getattr(win, "prediction" + str(i)).connect("clicked", on_prediction_click, i)
         i += 1
 
@@ -30,7 +42,7 @@ def on_prediction_click(widget, sn):
 
 win.connect("delete-event", Gtk.main_quit)
 
-createButtons()
+createButtons(buttonAmount)
 
 win.show_all()
 
