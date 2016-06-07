6b37bfe2d072580edb85f1f40fa585baee297f2b
Benjamin Dahl <benjadahl@gmail.com>
2016-06-07 08:57:16 +0200

    Made GUI code prettier (#3)
    * Made window always on top
    
    * Started development of menu-bar still a WIP
    
    * Testing out branches
    
    * Removed the menu testing from master branch
    
    * Made the loops pretier(converted to for loops)

--- GUI/LexicGUILauncher.py
+++ GUI/LexicGUILauncher.py
@@ -8,6 +8,42 @@ from gi.repository import Gtk
 #sys.path.insert(0, "../Predictor/")
 #import Predictor
 
+#Functions |---
+
+#Function for creating the buttons
+def createButtons(amount):
+    print ("[GUI] Creating " + str(amount) + " buttons")
+    for i in range(1, amount + 1):
+        #Add the buttons to the window class
+        setattr(win, "prediction" + str(i), Gtk.Button(label = "Prediction #" + str(i)))
+        #Add the buttons to the box
+        win.box.pack_start(getattr(win, "prediction" + str(i)), True, True, 0)
+        #Connect the buttons clicked event to the on_prediction_click function
+        getattr(win, "prediction" + str(i)).connect("clicked", on_prediction_click, i)
+
+#Function to update suggestions
+def updateSuggestions(suggestionList, amount):
+    print ("[GUI] Updating suggestions")
+    #Try to change the button labels (Will probably fail if the inputtet suggestions are not a list)
+    try:
+        #Run the loop for the amount of buttons
+        for i in range(0,amount + 1):
+            #If only attempt to change the labels,
+            if i < len(suggestionList):
+                #Get the button using getattr(), then set the label to the respective item of the list.
+                getattr(win, "prediction" + str(i + 1)).set_label(suggestionList[i])
+            else:
+                #Return if the length of the list has been reached
+                return
+    except:
+        print ("[GUI] Error during suggestion update (might be due to input not being a list)")
+
+def on_prediction_click(widget, sn):
+    print ("[GUI] Suggestion #" + str(sn) + " was pressed.")
+    #Return userchoice to script that emulates keypresses.
+
+#---|
+
 #Customizable variables |---
 
 #The amount of buttons that will be created:
@@ -21,51 +57,16 @@ class mainWindow (Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="LexicGUITest")
         self.set_keep_above(True)
+        self.set_decorated(False)
 
         #Create vertical box that the buttons will be placed in
         self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+
 
         #Add the box to the window
         self.add(self.box)
 
 win = mainWindow()
-
-#Function for creating the buttons
-def createButtons(amount):
-    print ("[GUI] Creating " + str(amount) + " buttons")
-    i = 1
-    while i <= amount:
-        #Add the buttons to the window class
-        setattr(win, "prediction" + str(i), Gtk.Button(label = "Prediction #" + str(i)))
-        #Add the buttons to the box
-        win.box.pack_start(getattr(win, "prediction" + str(i)), True, True, 0)
-        #Connect the buttons clicked event to the on_prediction_click function
-        getattr(win, "prediction" + str(i)).connect("clicked", on_prediction_click, i)
-        i += 1
-
-#Function to update suggestions
-def updateSuggestions(suggestionList, bAmount):
-    print ("[GUI] Updating suggestions")
-    #Try to change the button labels (Will probably fail if the inputtet suggestions are not a list)
-    try:
-        i = 0
-        #Run the loop for the amount of buttons
-        while i <= bAmount:
-            #If only attempt to change the labels,
-            if i < len(suggestionList):
-                #Get the button using getattr(), then set the label to the respective item of the list.
-                getattr(win, "prediction" + str(i + 1)).set_label(suggestionList[i])
-                i += 1
-            else:
-                #Return if the length of the list has been reached
-                return
-    except:
-        print ("[GUI] Error during suggestion update (might be due to input not being a list)")
-
-def on_prediction_click(widget, sn):
-    print ("[GUI] Suggestion #" + str(sn) + " was pressed.")
-    #Return userchoice to script that emulates keypresses.
-
 
 #Connect the closing of the main window, to ending the script with Gtk.main_quit
 win.connect("delete-event", Gtk.main_quit)
